5aceba8df226a4cc853c2ae572be9329d911b1a5
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-09-26 21:08:55 +0200

    Refactored result to use workload error.

--- AsyncWorkloads/Results/Result`.cs
+++ AsyncWorkloads/Results/Result`.cs
@@ -3,34 +3,34 @@ namespace AsyncWorkloads.Results;
 public readonly struct Result<TValue>
 {
     private readonly TValue? _value;
-    private readonly Exception? _exception;
+    private readonly WorkloadError? _error;
 
-    public bool IsSuccess => _exception is null;
+    public bool IsSuccess => _error is null;
 
-    private Result(TValue? value, Exception? exception)
+    private Result(TValue? value, WorkloadError? error)
     {
         _value = value;
-        _exception = exception;
+        _error = error;
     }
 
     public static Result<TValue> Success(TValue value)
         => new(value, null);
     
-    public static Result<TValue> Failure(Exception exception)
-        => new(default, exception);
+    public static Result<TValue> Failure(WorkloadError error)
+        => new(default, error);
 
-    public TReturn Match<TReturn>(Func<TValue, TReturn> value, Func<Exception, TReturn> exception)
-        => _exception is null
+    public TReturn Match<TReturn>(Func<TValue, TReturn> value, Func<WorkloadError, TReturn> error)
+        => _error is null
             ? value(_value!)
-            : exception(_exception);
+            : error((WorkloadError)_error);
     
     public Result<TResult> Map<TResult>(Func<TValue, TResult> map)
-        => _exception is null 
+        => _error is null 
             ? Result<TResult>.Success(map(_value!))
-            : Result<TResult>.Failure(_exception);
+            : Result<TResult>.Failure((WorkloadError)_error);
 
     public Result<TResult> Bind<TResult>(Func<TValue, Result<TResult>> bind)
-        => _exception is null
+        => _error is null
             ? bind(_value!)
-            : Result<TResult>.Failure(_exception);
+            : Result<TResult>.Failure((WorkloadError)_error);
 }
