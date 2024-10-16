7e8eab1fa4e6d5616007fef50611d7403da0fde6
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 16:48:34 +0200

    Added test to check if a type decorated with prerequisite workloads is itself an async workload.

--- AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
+++ AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
 using AsyncWorkloads.Attributes;
+using AsyncWorkloads.Workloads;
 
 namespace AsyncWorkloads.Analyzers.Tests;
 
@@ -58,6 +59,49 @@ public class AsyncWorkloadAnalyzerTests
         [AsyncWorkload<bool>("Test workload")]
         [{|AW002:PrerequisiteWorkload<TestPrerequisiteWorkload>|}]
         public class TestWorkload
+        {
+        }
+        """;
+        
+        // Verify
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ClassWithPrerequisiteWorkloadAttribute_ButNotAsyncWorkload_ShouldReportErrorDiagnostic()
+    {
+        // Arrange
+        WorkloadWithPrerequisitesAnalyzerTest test = new()
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net80
+        };
+        test.TestState.AdditionalReferences.Add(typeof(AsyncWorkloadAttribute<>).Assembly);
+        test.TestState.AdditionalReferences.Add(typeof(AsyncWorkload<>).Assembly);
+        test.TestState.AdditionalReferences.Add(typeof(Microsoft.Extensions.Logging.ILogger).Assembly);
+
+        // Expect
+        test.TestCode = /* lang=c#-test */
+        """
+        using System.Threading;
+        using System.Threading.Tasks;
+        using AsyncWorkloads.Attributes;
+        using AsyncWorkloads.Results;
+        using AsyncWorkloads.Workloads;
+        using Microsoft.Extensions.Logging;
+
+        public class TestPrerequisiteWorkload : AsyncWorkload<int>
+        {
+            public TestPrerequisiteWorkload(ILogger<AsyncWorkload<int>> logger)
+                : base("Test workload", logger)
+            {
+            }
+
+            protected override Task<WorkloadResult<int>> ExecuteWorkAsync(CorrelationId correlationId, CancellationToken cancellationToken)
+                => Task.FromResult(Success(69, correlationId));
+        }
+
+        [PrerequisiteWorkload<TestPrerequisiteWorkload>]
+        public class {|AW003:TestWorkload|}
         {
         }
         """;
