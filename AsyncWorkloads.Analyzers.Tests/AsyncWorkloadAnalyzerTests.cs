9e314472adc7d4b605d101e33890c15161f2347e
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 13:29:11 +0200

    Fixed project references inside analyzer unit test.

--- AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
+++ AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
+using AsyncWorkloads.Attributes;
 
 namespace AsyncWorkloads.Analyzers.Tests;
 
@@ -14,16 +15,24 @@ public class AsyncWorkloadAnalyzerTests
         // Arrange
         string source = /* lang=c#-test */
         """
+        using AsyncWorkloads.Attributes;
+
         [AsyncWorkload<bool>("Test workload")]
         public class TestWorkload
         {
         }
         """;
-        AsyncWorkloadAnalyzerTest test = new() { TestCode = source };
+
+        AsyncWorkloadAnalyzerTest test = new()
+        {
+            TestCode = source,
+            ReferenceAssemblies = ReferenceAssemblies.NetStandard.NetStandard20
+        };
+        test.TestState.AdditionalReferences.Add(typeof(AsyncWorkloadAttribute<bool>).Assembly);
 
         // Expect
         DiagnosticResult partialOrSealedError = new DiagnosticResult(AsyncWorkloadAnalyzer.DiagnosticId, DiagnosticSeverity.Error)
-                .WithSpan(3, 26, 3, 39)
+                .WithSpan(4, 14, 4, 26)
                 .WithArguments("TestWorkload");
         test.ExpectedDiagnostics.Add(partialOrSealedError);
         
