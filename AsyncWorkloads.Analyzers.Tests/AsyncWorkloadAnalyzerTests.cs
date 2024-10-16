a6715a319ccb0114111d331ccec23467f64967cf
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 12:03:22 +0200

    Added reference to analyzers project and defined first unit test.

--- AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
+++ AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
@@ -1,10 +1,30 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+
 namespace AsyncWorkloads.Analyzers.Tests;
 
 public class AsyncWorkloadAnalyzerTests
 {
     [Fact]
-    public void Test1()
+    public async Task ClassWithoutSealedOrPartial_WhenDecoratedWithAsyncWorkloadAttribute_ShouldReportErrorDiagnostic()
     {
+        // Arrange
+        string source =
+        """
+        [AsyncWorkload<bool>("Test workload")]
+        public class TestWorkload
+        {
+        }
+        """;
+        CSharpAnalyzerTest<AsyncWorkloadAnalyzer, DefaultVerifier> test = new() { TestCode = source };
+        test.ExpectedDiagnostics.Add(
+            new DiagnosticResult(AsyncWorkloadAnalyzer.DiagnosticId, DiagnosticSeverity.Error)
+                .WithSpan(3, 26, 3, 39)
+                .WithArguments("TestWorkload")
+            );
 
+        // Act
+        await test.RunAsync();
     }
 }
