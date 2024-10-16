d2917964a896df1098175cc360673eee12064ff4
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 13:07:04 +0200

    Added name property to AsyncWorkloadAttribute.

--- AsyncWorkloads.Attributes/AsyncWorkloadAttribute.cs
+++ AsyncWorkloads.Attributes/AsyncWorkloadAttribute.cs
@@ -5,5 +5,10 @@ namespace AsyncWorkloads.Attributes;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 public class AsyncWorkloadAttribute<TValue> : Attribute
 {
-    
+    public string Name { get; }
+
+    public AsyncWorkloadAttribute(string name)
+    {
+        Name = name;
+    }
 }
