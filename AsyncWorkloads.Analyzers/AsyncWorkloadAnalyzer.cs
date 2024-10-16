4d3189e4a41c882ca60a32ef71ae4b8f025e2703
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 11:27:01 +0200

    Added analyzer class.

---
+++ AsyncWorkloads.Analyzers/AsyncWorkloadAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AsyncWorkloads.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AsyncWorkloadAnalyzer : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => throw new NotImplementedException();
+
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AsyncWorkloads.Analyzers/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AsyncWorkloads.Analyzers
-{
-    public class Class1
-    {
-
-    }
-}
