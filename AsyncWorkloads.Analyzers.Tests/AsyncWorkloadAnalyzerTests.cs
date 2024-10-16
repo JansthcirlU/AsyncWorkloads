27a1ace0059b6997a5f3225a6ec4a37330486a0b
Jan Ulrichts <jan.ulrichts@gmail.com>
2024-10-16 12:52:49 +0200

    Cleaned up test definitions.

--- AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
+++ AsyncWorkloads.Analyzers.Tests/AsyncWorkloadAnalyzerTests.cs
@@ -4,27 +4,30 @@ using Microsoft.CodeAnalysis.Testing;
 
 namespace AsyncWorkloads.Analyzers.Tests;
 
+using AsyncWorkloadAnalyzerTest = CSharpAnalyzerTest<AsyncWorkloadAnalyzer, DefaultVerifier>;
+
 public class AsyncWorkloadAnalyzerTests
 {
     [Fact]
     public async Task ClassWithoutSealedOrPartial_WhenDecoratedWithAsyncWorkloadAttribute_ShouldReportErrorDiagnostic()
     {
         // Arrange
-        string source =
+        string source = /* lang=c#-test */
         """
         [AsyncWorkload<bool>("Test workload")]
         public class TestWorkload
         {
         }
         """;
-        CSharpAnalyzerTest<AsyncWorkloadAnalyzer, DefaultVerifier> test = new() { TestCode = source };
-        test.ExpectedDiagnostics.Add(
-            new DiagnosticResult(AsyncWorkloadAnalyzer.DiagnosticId, DiagnosticSeverity.Error)
-                .WithSpan(3, 26, 3, 39)
-                .WithArguments("TestWorkload")
-            );
+        AsyncWorkloadAnalyzerTest test = new() { TestCode = source };
 
-        // Act
+        // Expect
+        DiagnosticResult partialOrSealedError = new DiagnosticResult(AsyncWorkloadAnalyzer.DiagnosticId, DiagnosticSeverity.Error)
+                .WithSpan(3, 26, 3, 39)
+                .WithArguments("TestWorkload");
+        test.ExpectedDiagnostics.Add(partialOrSealedError);
+        
+        // Verify
         await test.RunAsync();
     }
 }
